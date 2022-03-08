d40801717ac1e401a3e17ac1067cc6011717cfd5
Justin Cano <jstncno@gmail.com>
2022-03-08 01:14:08 -0800

    Removed unused assets

--- src/game/MainScene.ts
+++ src/game/MainScene.ts
@@ -7,8 +7,6 @@ export default class MainScene extends Phaser.Scene {
 
   preload() {
     this.load.image('sky', '/assets/sky.png');
-    this.load.image('star', '/assets/star.png');
-    this.load.image('bomb', '/assets/bomb.png');
     this.load.spritesheet('bladekeeper', '/assets/metal_bladekeeper_spritesheet.png', { frameWidth: 64, frameHeight: 64 });
   }
 
