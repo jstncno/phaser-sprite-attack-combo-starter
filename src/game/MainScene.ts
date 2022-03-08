feaa9e5e7a2e139ed9aceacd9989d793482e4614
Justin Cano <jstncno@gmail.com>
2022-03-08 00:59:13 -0800

    Basic platformer controls

--- src/game/MainScene.ts
+++ src/game/MainScene.ts
@@ -1,17 +1,15 @@
 import Phaser from 'phaser';
-import { get } from 'svelte/store';
-
-import * as GameState from 'src/game/state';
 
 export default class MainScene extends Phaser.Scene {
   private platforms?: Phaser.Physics.Arcade.StaticGroup;
   private player?: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
   private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
-  private attackKey?: Phaser.Input.Keyboard.Key;
 
   preload() {
     this.load.image('sky', '/assets/sky.png');
-    this.load.spritesheet('warrior', '/assets/warrior_spritesheet.png', { frameWidth: 69, frameHeight: 44 });
+    this.load.image('star', '/assets/star.png');
+    this.load.image('bomb', '/assets/bomb.png');
+    this.load.spritesheet('bladekeeper', '/assets/metal_bladekeeper_spritesheet.png', { frameWidth: 64, frameHeight: 64 });
   }
 
   create() {
@@ -22,33 +20,21 @@ export default class MainScene extends Phaser.Scene {
   }
 
   update(time: number, delta: number) {
-    if (!this.player) return;
-
-    const state = get(GameState.playerAnimationState);
-
-    if (state.animation === 'attack') {
-      this.player.setVelocityX(0);
-      return;
-    }
-
-    if (this.cursors?.left.isDown) {
-      this.player.setVelocityX(-300);
-      this.player.anims.play('run', /* ignoreIfPlaying */ true);
-      if (state.direction === 'right') this.player.flipX = true;
-      GameState.playerAnimationState.set({...state, animation: 'run', direction: 'left'});
-    } else if (this.cursors?.right.isDown) {
-      this.player.setVelocityX(300);
-      this.player.anims.play('run', /* ignoreIfPlaying */ true);
-      if (state.direction === 'left') this.player.flipX = false;
-      GameState.playerAnimationState.set({...state, animation: 'run', direction: 'right'});
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-200);
+      this.player.anims.play('run', true);
+      this.player.flipX = true;
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(200);
+      this.player.anims.play('run', true);
+      this.player.flipX = false;
     } else {
       this.player.setVelocityX(0);
       this.player.anims.play('idle', /* ignoreIfPlaying */ true);
-      GameState.playerAnimationState.set({...state, animation: 'idle'});
     }
 
-    if (this.cursors?.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-300);
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
     }
   }
 
@@ -56,10 +42,11 @@ export default class MainScene extends Phaser.Scene {
   private createLevel() {
     this.createBackground();
     this.createPlatforms();
+    this.setupControls();
   }
 
   private createBackground() {
-    // A simple background for our game
+    //  A simple background for our game
     this.add.image(400, 300, 'sky');
   }
 
@@ -74,43 +61,29 @@ export default class MainScene extends Phaser.Scene {
     this.platforms.add(ground);
   }
 
-
   private createPlayer() {
     // The player and its settings
-    this.player = this.physics.add.sprite(100, 450, 'warrior').setScale(2.5);
+    this.player = this.physics.add.sprite(100, 450, 'bladekeeper').setScale(2);
+    // Player physics properties. Give the little guy a slight bounce.
     this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
 
     // Our player animations, turning, walking left and walking right.
     this.anims.create({
-      key: 'idle',
-      frames: this.anims.generateFrameNumbers('warrior', {start: 0, end: 5}),
-      frameRate: 10,
+      key: 'run',
+      frames: this.anims.generateFrameNumbers('bladekeeper', { start: 8, end: 15 }),
+      frameRate: 15,
       repeat: -1,
     });
     this.anims.create({
-      key: 'run',
-      frames: this.anims.generateFrameNumbers('warrior', {start: 6, end: 13}),
-      frameRate: 20,
+      key: 'idle',
+      frames: this.anims.generateFrameNumbers('bladekeeper', { start: 0, end: 7 }),
+      frameRate: 10,
       repeat: -1,
-    });
-    const attackAnim = this.anims.create({
-      key: 'attack',
-      frames: this.anims.generateFrameNumbers('warrior', {start: 14, end: 21}),
-      duration: 300,
-    });
-    if (attackAnim) {
-      attackAnim.addFrame([{key: 'warrior', frame: 0}]);
-    }
-
-    this.player.on('animationcomplete', (anim: Phaser.Animations.Animation, frame: Phaser.Animations.AnimationFrame, sprite: Phaser.GameObjects.Sprite) => {
-      const state = get(GameState.playerAnimationState);
-      if (anim.key === 'attack') GameState.playerAnimationState.set({...state, animation: 'idle'});
     });
   }
 
   private setupCollisionEvents() {
-    if (!this.player || !this.platforms) return;
     // Collide the player and the stars with the platforms
     this.physics.add.collider(this.player, this.platforms);
   }
@@ -118,12 +91,5 @@ export default class MainScene extends Phaser.Scene {
   private setupControls() {
     //  Input Events
     this.cursors = this.input.keyboard.createCursorKeys();
-
-    this.attackKey = this.input.keyboard.addKey('SPACE');
-    this.attackKey.on('down', () => {
-      const state = get(GameState.playerAnimationState);
-      this.player?.anims.play('attack', /* ignoreIfPlaying */ true);
-      GameState.playerAnimationState.set({...state, animation: 'attack'});
-    });
   }
 }
