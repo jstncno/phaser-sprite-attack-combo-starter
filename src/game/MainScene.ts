7761da857844c16a1509880edc33b465ee0f50bb
Justin Cano <jstncno@gmail.com>
2022-03-08 18:23:25 -0800

    Added state machine for controlling animation

--- src/game/MainScene.ts
+++ src/game/MainScene.ts
@@ -1,9 +1,13 @@
 import Phaser from 'phaser';
+import { get, writable } from 'svelte/store';
+
+import machine, { Event, StateName } from './state-machines/PlayerAnimationStateMachine';
 
 export default class MainScene extends Phaser.Scene {
   private platforms?: Phaser.Physics.Arcade.StaticGroup;
   private player?: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
   private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
+  private position = writable({x: 100, y: 450});
 
   preload() {
     this.load.image('sky', '/assets/sky.png');
@@ -18,22 +22,41 @@ export default class MainScene extends Phaser.Scene {
   }
 
   update(time: number, delta: number) {
-    if (this.cursors.left.isDown) {
-      this.player.setVelocityX(-200);
-      this.player.anims.play('run', true);
-      this.player.flipX = true;
-    } else if (this.cursors.right.isDown) {
-      this.player.setVelocityX(200);
-      this.player.anims.play('run', true);
-      this.player.flipX = false;
-    } else {
-      this.player.setVelocityX(0);
-      this.player.anims.play('idle', /* ignoreIfPlaying */ true);
-    }
 
-    if (this.cursors.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-330);
+    const state = get(machine.currentState);
+    console.log(StateName[state.name]);
+
+    switch (state.name) {
+      case StateName.LEFT:
+      case StateName.RIGHT:
+        this.runRunState();
+        break;
+      // case StateName.ATTACK_1_ANTICIPATION:
+      //   break;
+      case StateName.JUMPING:
+        this.runJumpingState();
+        if (this.player.body.touching.down) {
+          this.player.setVelocityY(-330);
+        } else {
+          const {y} = get(this.position);
+          if (y < this.player.body.y) {
+            machine.triggerEvent(Event.FALL);
+          }
+        }
+        break;
+      case StateName.LANDING:
+        this.runLandingState();
+        if (this.player.body.touching.down) {
+          machine.triggerEvent(Event.LAND);
+        }
+        break;
+      case StateName.IDLE:
+      default:
+        this.runIdleState();
+        break;
     }
+    this.position.set({x: this.player.body.x, y: this.player.body.y});
+    this.checkInput();
   }
 
 
@@ -60,8 +83,9 @@ export default class MainScene extends Phaser.Scene {
   }
 
   private createPlayer() {
+    const {x, y} = get(this.position);
     // The player and its settings
-    this.player = this.physics.add.sprite(100, 450, 'bladekeeper').setScale(2);
+    this.player = this.physics.add.sprite(x, y, 'bladekeeper').setScale(2);
     // Player physics properties. Give the little guy a slight bounce.
     this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
@@ -79,6 +103,23 @@ export default class MainScene extends Phaser.Scene {
       frameRate: 10,
       repeat: -1,
     });
+    this.anims.create({
+      key: 'jump',
+      frames: this.anims.generateFrameNumbers('bladekeeper', { start: 17, end: 21 }),
+      frameRate: 10,
+    });
+    this.anims.create({
+      key: 'fall',
+      frames: this.anims.generateFrameNumbers('bladekeeper', { start: 22, end: 25 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+
+    this.player.on('animationcomplete', (anim: Phaser.Animations.Animation, frame: Phaser.Animations.AnimationFrame, sprite: Phaser.GameObjects.Sprite) => {
+      // switch (anim.key) {}
+      console.log(anim);
+    });
   }
 
   private setupCollisionEvents() {
@@ -89,5 +130,43 @@ export default class MainScene extends Phaser.Scene {
   private setupControls() {
     //  Input Events
     this.cursors = this.input.keyboard.createCursorKeys();
+    this.cursors.left.addListener('up', () => machine.triggerEvent(Event.LEFT_UP));
+    this.cursors.right.addListener('up', () => machine.triggerEvent(Event.RIGHT_UP));
+    this.cursors.up.addListener('down', () => machine.triggerEvent(Event.JUMP));
+    this.cursors.space.addListener('down', () => machine.triggerEvent(Event.ATTACK));
+  }
+
+  private checkInput() {
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-200);
+      this.player.flipX = true;
+      machine.triggerEvent(Event.LEFT);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(200);
+      this.player.flipX = false;
+      machine.triggerEvent(Event.RIGHT);
+    }
+  }
+
+
+  /*****************/
+  /* State Methods */
+  /*****************/
+
+  private runIdleState() {
+    this.player.anims.play('idle', /* ignoreIfPlaying */ true);
+    this.player.setVelocityX(0);
+  }
+
+  private runRunState() {
+    this.player.anims.play('run', /* ignoreIfPlaying */ true);
+  }
+
+  private runJumpingState() {
+    this.player.anims.play('jump', /* ignoreIfPlaying */ true);
+  }
+
+  private runLandingState() {
+    this.player.anims.play('fall', /* ignoreIfPlaying */ true);
   }
 }
---
+++ src/game/state-machines/PlayerAnimationStateMachine.ts
@@ -0,0 +1,157 @@
+import { writable, Writable, get } from 'svelte/store';
+
+export enum StateName {
+  IDLE,
+  LEFT,
+  RIGHT,
+  JUMPING,
+  LANDING,
+
+  ATTACK_1_ANTICIPATION,
+  ATTACK_1_CONTACT,
+  ATTACK_1_RECOVERY,
+
+  ATTACK_2_ANTICIPATION,
+  ATTACK_2_CONTACT,
+  ATTACK_2_RECOVERY,
+
+  ATTACK_3_ANTICIPATION,
+  ATTACK_3_CONTACT,
+  ATTACK_3_RECOVERY,
+}
+
+export enum Event {
+  JUMP,
+  LAND,
+  FALL,
+  LEFT,
+  LEFT_UP,
+  RIGHT,
+  RIGHT_UP,
+  ATTACK,
+}
+
+interface StateMachine {
+  readonly states: State[];
+  readonly stateNameMapping: Map<StateName, State>;
+  readonly eventTransitions: Map<Event, Map<StateName, State>>;
+  currentState: Writable<State>;
+  triggerEvent(event: Event): this;
+  addEventTransition(event: Event, from: StateName, to: StateName): boolean;
+  setState(name: StateName): this;
+}
+
+class StateMachine implements StateMachine {
+  readonly stateNameMapping = new Map<StateName, State>();
+  readonly eventTransitions = new Map<Event, Map<StateName, State>>();
+  constructor(readonly states: State[]) {
+    states.forEach(state => this.stateNameMapping.set(state.name, state));
+    this.currentState = writable(states[0]);
+  }
+  triggerEvent(event: Event): this {
+    const eventTransition = this.eventTransitions.get(event);
+    // console.log(Event[event], eventTransition)
+    if (!eventTransition) return this;
+    const currentState = get(this.currentState);
+    // console.log(currentState);
+    const nextState = eventTransition.get(currentState.name);
+    // console.log(nextState);
+    if (!nextState) return this;
+    this.transitionTo(nextState);
+    return this;
+  }
+  addEventTransition(event: Event, from: StateName, to: StateName): boolean {
+    const fromState = this.stateNameMapping.get(from);
+    if (!fromState) {
+      console.error(`"From" state ${StateName[from]} not found`);
+      return false;
+    }
+    const toState = this.stateNameMapping.get(to);
+    if (!toState) {
+      console.error(`"To" state ${StateName[to]} not found`);
+      return false;
+    }
+    if (!this.eventTransitions.has(event)) {
+      this.eventTransitions.set(event, new Map<StateName, State>());
+    }
+    this.eventTransitions.get(event)!.set(from, toState);
+    return true;
+  }
+  setState(name: StateName): this {
+    const nextState = this.stateNameMapping.get(name);
+    if (nextState) return this.transitionTo(nextState);
+    return this;
+  }
+
+  private transitionTo(state: State): this {
+    const currentState = get(this.currentState);
+    currentState.onLeave();
+    this.currentState.set(state);
+    state.onEnter();
+    if (!state.next) return this;
+    const nextState = this.stateNameMapping.get(state.next);
+    if (nextState) return this.transitionTo(nextState);
+    return this;
+  }
+}
+
+interface State {
+  readonly name: StateName;
+  next?: StateName;
+  onEnter(): void;
+  onLeave(): void;
+  setNext(stateName: StateName): this;
+}
+
+class State implements State {
+  readonly eventTransitions = new Map<string, string>();
+  constructor(readonly name: StateName) {}
+  onEnter() {}
+  onLeave() {}
+  setNext(stateName: StateName): this {
+    this.next = stateName;
+    return this;
+  }
+}
+
+const states = [
+  new State(StateName.IDLE),
+  new State(StateName.LEFT),
+  new State(StateName.RIGHT),
+  new State(StateName.JUMPING),
+  new State(StateName.LANDING),
+  new State(StateName.IDLE),
+
+  new State(StateName.ATTACK_1_ANTICIPATION),
+  new State(StateName.ATTACK_1_CONTACT),
+  new State(StateName.ATTACK_1_RECOVERY),
+
+  new State(StateName.ATTACK_2_RECOVERY),
+  new State(StateName.ATTACK_2_CONTACT),
+  new State(StateName.ATTACK_2_RECOVERY),
+
+  new State(StateName.ATTACK_3_ANTICIPATION),
+  new State(StateName.ATTACK_1_CONTACT),
+  new State(StateName.ATTACK_3_RECOVERY),
+];
+
+const machine = new StateMachine(states);
+machine.addEventTransition(Event.LEFT, StateName.IDLE, StateName.LEFT);
+machine.addEventTransition(Event.LEFT, StateName.RIGHT, StateName.LEFT);
+machine.addEventTransition(Event.LEFT, StateName.JUMPING, StateName.LEFT);
+machine.addEventTransition(Event.LEFT_UP, StateName.LEFT, StateName.IDLE);
+machine.addEventTransition(Event.RIGHT, StateName.IDLE, StateName.RIGHT);
+machine.addEventTransition(Event.RIGHT, StateName.LEFT, StateName.RIGHT);
+machine.addEventTransition(Event.RIGHT, StateName.JUMPING, StateName.RIGHT);
+machine.addEventTransition(Event.RIGHT_UP, StateName.RIGHT, StateName.IDLE);
+machine.addEventTransition(Event.JUMP, StateName.IDLE, StateName.JUMPING);
+machine.addEventTransition(Event.JUMP, StateName.LEFT, StateName.JUMPING);
+machine.addEventTransition(Event.JUMP, StateName.RIGHT, StateName.JUMPING);
+machine.addEventTransition(Event.FALL, StateName.JUMPING, StateName.LANDING);
+machine.addEventTransition(Event.LAND, StateName.LANDING, StateName.IDLE);
+machine.addEventTransition(Event.ATTACK, StateName.IDLE, StateName.ATTACK_1_ANTICIPATION);
+// machine.addEventTransition(Event.ATTACK, 'attack_1_contact', 'attack_2_aniticipation');
+// machine.addEventTransition(Event.ATTACK, 'attack_2_contact', 'attack_3_aniticipation');
+console.log(machine.eventTransitions);
+
+export default machine;
